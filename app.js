922d6d4f57ba8ac887183ec78ad533f98135891d
4614s <onurgral1446.2@gmail.com>
2025-07-10 12:20:17 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+if(!location.search.includes("port:186092")){
+  document.title = `Err: ${location.host} Bağlanmayı reddetti.`;
+  document.body.style.background = "white";
+  document.body.innerHTML = `<b style="color: red">Hata:</b> <br/>
+  ${location.host} Bağlanmayı reddetti.`;
+  document.body.style.background = "white";
+  document.body.style.color = "black";
+  document.body.style.textAlign = "center";
+} else {
+  
+function whatsappYonlendir() {
+ setTimeout( () => { location.href = "https://wa.me/905468151446?text=Merhaba%2C%20size%20web%20sayfanızdan%20ulaştım.";
+ },250)
+}
+
+let guncelYil = new Date().getFullYear();
+
+document.querySelector("#copyright").innerText = `© ${guncelYil} Onur Oto Kurtarma — Tüm Hakları Saklıdır.`;
+
+
+document.querySelectorAll("img").forEach(
+  (foto) => {
+    foto.addEventListener("error", () => {
+      foto.style.display = `none`;
+    });
+  });
+  
+document.querySelector(".foto2").addEventListener("dblclick", () => {
+  alert("Ne yapmaya çalışıyorsun?")
+});
+}
