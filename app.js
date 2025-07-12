d19c602b7cc71e414087e4558d6064e0418b1892
4614s <onurgral1446.2@gmail.com>
2025-07-12 13:54:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-if(!location.search.includes("port:186092")){
-  document.title = `Err: ${location.host} Bağlanmayı reddetti.`;
-  document.body.style.background = "white";
-  document.body.innerHTML = `<b style="color: red">Hata:</b> <br/>
-  ${location.host} Bağlanmayı reddetti.`;
-  document.body.style.background = "white";
-  document.body.style.color = "black";
-  document.body.style.textAlign = "center";
-} else {
+if(location.search.includes("port:186092")){  
+ location.search = "?port=186092";
+}
   
+else if (location.search.includes("port=186092")) {  
 function whatsappYonlendir() {
  setTimeout( () => { location.href = "https://wa.me/905468151446?text=Merhaba%2C%20size%20web%20sayfanızdan%20ulaştım.";
  },250)
@@ -28,4 +23,14 @@ document.querySelectorAll("img").forEach(
 document.querySelector(".foto2").addEventListener("dblclick", () => {
   alert("Ne yapmaya çalışıyorsun?")
 });
+}
+else {
+  document.title = `Err: ${location.host} Bağlanmayı reddetti.`;
+  document.body.style.background = "white";
+  document.body.innerHTML = `<b style="color: red">Hata:</b> <br/>
+  ${location.host} Bağlanmayı reddetti.`;
+  document.body.style.background = "white";
+  document.body.style.color = "black";
+  document.body.style.textAlign = "center";
+  
 }
